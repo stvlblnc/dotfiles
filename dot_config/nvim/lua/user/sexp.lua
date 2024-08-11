c7e952523070ae5ae2097357410a010699b93e2f
Steve Leblanc <stvlblnc@gmail.com>
2024-08-10 21:54:33 -0400

    Remove bogus configurartion files.

--- dot_config/nvim/lua/user/sexp.lua
+++
@@ -1,6 +0,0 @@
-local status_ok, sexp = pcall(require, "sexp")
-if not status_ok then
-   return
-end
-
-vim.g.sexp_filetypes = "clojure,scheme,lisp,timl,fennel,janet"
--- dot_config/nvim/lua/user/textobjs.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, various_textobjs = pcall(require, "various-textobjs")
-if not status_ok then
-  vim.notify("Error requiring various-textobjs")
-  return
-end
-
-various_textobjs.setup{}

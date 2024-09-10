e0be770cad7febcda9ad651e2c38afb5009d5787
Steve Leblanc <stvlblnc@gmail.com>
2024-09-10 11:48:32 -0400

    Implement the full suite of mappings.

--- dot_config/nvim/lua/steve/plugins/substitute.lua
+++ dot_config/nvim/lua/steve/plugins/substitute.lua
@@ -3,14 +3,25 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local substitute = require("substitute")
+    local range = require("substitute.range")
+    local exchange = require("substitute.exchange")
 
     substitute.setup()
 
     local keymap = vim.keymap
 
-    keymap.set("n", "gs", substitute.operator, { desc = "Substitute with a motion" })
-    keymap.set("n", "gss", substitute.line, { desc = "Substitute line" })
-    keymap.set("n", "gS", substitute.eol, { desc = "Substitute to end of line" })
-    keymap.set("x", "gs", substitute.visual, { desc = "Substitute in visual mode" })
+    keymap.set("n", "s", substitute.operator, { noremap = true, desc = "Substitute with a motion" })
+    keymap.set("n", "ss", substitute.line, { noremap = true, desc = "Substitute line" })
+    keymap.set("n", "S", substitute.eol, { noremap = true, desc = "Substitute to end of line" })
+    keymap.set("x", "s", substitute.visual, { noremap = true, desc = "Substitute in visual mode" })
+
+    keymap.set("n", "<Leader>s", range.operator, { noremap = true, desc = "Substitute with a motion" })
+    keymap.set("x", "<Leader>s", range.visual, { noremap = true, desc = "Substitute line" })
+    keymap.set("n", "<Leader>ss", range.word, { noremap = true, desc = "Substitute to end of line" })
+
+    keymap.set("n", "<Leader>sx", exchange.operator, { noremap = true, desc = "Exchange with a motion" })
+    keymap.set("n", "<Leader>sxx", exchange.line, { noremap = true, desc = "Exchange line" })
+    keymap.set("x", "<Leader>X", exchange.visual, { noremap = true, desc = "Exchange in visual mode" })
+    keymap.set("n", "<Leader>sxc", exchange.clear, { noremap = true, desc = "Clear exchange selection" })
   end,
 }

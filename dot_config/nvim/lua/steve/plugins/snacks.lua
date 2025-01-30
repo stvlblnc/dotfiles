2c32b9998e62d2ba3940c3b8dd130e54060a5e45
Steve Leblanc <stvlblnc@gmail.com>
2025-01-30 17:12:01 -0500

    Add snacks.nvim

---
+++ dot_config/nvim/lua/steve/plugins/snacks.lua
@@ -0,0 +1,79 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    styles = {
+      notification = {
+        wo = { wrap = true }, -- wrap notifications
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>un",
+      function()
+        Snacks.notifier.hide()
+      end,
+      desc = "Dismiss all notifications",
+    },
+    {
+      "<leader>bd",
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = "Delete buffer",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>gb",
+      function()
+        Snacks.git.blame_line()
+      end,
+      desc = "Git blame line",
+    },
+    {
+      "<leader>gB",
+      function()
+        Snacks.git.browse()
+      end,
+      desc = "Git browse",
+    },
+    {
+      "<leader>gf",
+      function()
+        Snacks.lazygit.log_file()
+      end,
+      desc = "Lazygit current file history",
+    },
+    {
+      "<leader>gl",
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = "Lazygit log (cwd)",
+    },
+    {
+      "<leader>cR",
+      function()
+        Snacks.rename.rename_file()
+      end,
+      desc = "Rename file",
+    },
+  },
+}
